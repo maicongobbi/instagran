9a71c9b87d0299fb149f90a2217018b9d8b335bb
maicongobbi <maicongobbi@gmail.com>
2019-01-25 18:30:46 -0300

    correcao public

---
+++ src/app/acesso/acessoComponent.ts
@@ -0,0 +1,67 @@
+import { animate, keyframes } from '@angular/animations';
+import { style, transition, state } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { trigger } from '@angular/animations';
+
+@Component({
+  selector: 'app-acesso',
+  templateUrl: './acesso.component.html',
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-150px, 0)' }),
+        animate('500ms 0s ease-in-out') //duração, delay e aceleração
+      ]) // estado void é quando ainda o componente não foi criado
+       // duração de 500, 0 é o atraso para inicio da transição
+    ]),
+    trigger('animacao-painel', [ // esse nome será associado ao html onde será usado - VOID É O ESTADO INICAL do componente
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(50px, 0)' }), //inicialmente nesta posicao
+
+        //0 void ----X----------------------------X--X--X-------------------X criado 1.5s//
+        animate('1.5s 0s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 0.6, transform: 'translateX(0)'}),
+          style({ offset: 0.86, opacity: 0.7, transform: 'translateX(0)'}),
+
+          style({ offset: 0.88, opacity: 0.81, transform: 'translateY(-10px)'}),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)'}),
+          style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)'}),
+          style({ offset: 0.94, opacity: 1, transform: 'translateY(10px)'}),
+          style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)'}),
+          style({ offset: 0.98, opacity: 1, transform: 'translateY(10px)'})
+        ])) //duração, delay e aceleração
+        // offset  vai de 0 - 1 % ou seja na porcentagem X ele realiza algo
+      ])
+    ])
+  ]
+})
+export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado'
+  public estadoPainel: string = 'criado'
+  public cadastro: boolean = false
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public exibirPainel(event: string) : void {
+    this.cadastro = event === 'cadastro' ? true : false
+  }
+
+  public inicioDaAnimacao(): void {
+ /*    console.log('início da animação') */
+  }
+
+  public fimDaAnimacao(): void {
+/*     console.log('fim da animação') */
+  }
+}
